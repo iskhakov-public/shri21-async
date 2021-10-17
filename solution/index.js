b102c4d0b71588a0dce051a003b4b30354814b5a
Nail Iskhakov <nail@iskhakov.me>
2021-10-17 13:36:42 +0300

    solution fixed

--- solution/index.js
+++ solution/index.js
@@ -1,37 +1,36 @@
 // solution/index.js
 module.exports = function (Homework) {
+  // async call
+  async function ac(fn, ...args) {
+    const cb = () => {
+      return new Promise((resolve) => {
+        fn(...args, (result) => resolve(result));
+      });
+    };
 
-    // async call
-    async function ac(fn, ...args) {
-        const cb = () => {
-            return new Promise((resolve) => {
-                fn(...args, (result) => resolve(result))
-            })
-        }
+    return await cb();
+  }
 
-        return await cb()
-    }
+  return (array, fn, initialValue, cb) => {
+    fnc = async () => {
+      size = await ac(array.length);
+      // console.log("length", size)
+      // i = await ac(add, 5, 10)
+      // console.log("i is", i)
+      i = 0;
+      let elem;
+      let accumulator = initialValue;
+      // condition = await ac(lessOrEqual, i, size)
+      // console.log("condition", condition)/
+      while (await ac(Homework.less, i, size)) {
+        elem = await ac(array.get, i);
+        accumulator = await ac(fn, accumulator, elem, i, array);
+        i = await ac(Homework.add, i, 1);
+        // console.log(elem, accumulator, i);
+      }
+      return accumulator;
+    };
 
-    return (array, fn, initialValue, cb) => {
-        fnc = async () => {
-            size = await ac(asyncArray.length)
-            // console.log("length", size)
-            // i = await ac(add, 5, 10)
-            // console.log("i is", i)
-            i = 0
-            let elem
-            let accumulator = initialValue
-            // condition = await ac(lessOrEqual, i, size)
-            // console.log("condition", condition)/
-            while (await ac(less, i, size)) {
-                elem = await ac(asyncArray.get, i)
-                accumulator = await ac(fn, accumulator, elem, i, asyncArray)
-                i = await ac(add, i, 1)
-                console.log(elem, accumulator, i)
-            }
-            return accumulator
-        }
-    
-        fnc().then(res => cb(res))
-    }
-}
+    fnc().then((res) => cb(res));
+  };
+};
